542aaef0b75837a4162dfde404f9f395c6c25398
PHTÿ <132127310+PHTYakumo@users.noreply.github.com>
2026-04-14 02:46:57 +0800

    Optimize the drop of gold

--- files/drop_money_updated.lua
+++ files/drop_money_updated.lua
@@ -71,38 +71,50 @@ function do_money_drop( amount_multiplier, trick_kill )
 		if ComponentGetValue2( comp_worldstate, "perk_gold_is_forever" ) and ( money > 10 or is_blood_money ) then remove_timer_level = 2 end
 	end
 
-	local d_dollar = entity
-	
-	if money >= 1000*1000 then
-		d_dollar = de_load_gold_entity( "data/entities/items/pickup/gold_giant_dollar.xml", x, y-8, remove_timer_level * 4 )
-	elseif money >= 100*1000 then
-		d_dollar = de_load_gold_entity( "data/entities/items/pickup/gold_dollar.xml", x, y-8, remove_timer_level * 3.5 )
-	elseif money >= 10*1000 then
-		d_dollar = de_load_gold_entity( gold_entity .. "10000.xml", x, y-8, remove_timer_level * 3 )
-	elseif money >= 1000 then
-		d_dollar = de_load_gold_entity( gold_entity .. "1000.xml", x, y-8, remove_timer_level * 2.5 )
-	elseif money >= 200 then
-		d_dollar = de_load_gold_entity( gold_entity .. "200.xml", x, y-8, remove_timer_level * 2 )
-	elseif money >= 50 then
-		d_dollar = de_load_gold_entity( gold_entity .. "50.xml", x, y-8, remove_timer_level * 1.5 )
+	if DEEP_END_MONEY_CD == nil then DEEP_END_MONEY_CD = 0 end
+	if DEEP_END_MONEY_CACHE == nil then DEEP_END_MONEY_CACHE = 0 end
+
+	if GameGetFrameNum() - DEEP_END_MONEY_CD < 30 then
+		DEEP_END_MONEY_CACHE = money + DEEP_END_MONEY_CACHE 
 	else
-		d_dollar = de_load_gold_entity( gold_entity .. "10.xml", x, y-8, remove_timer_level )
-	end
+		money = money + DEEP_END_MONEY_CACHE
+		DEEP_END_MONEY_CACHE = 0
 
-	local dollar_comps = EntityGetComponent( d_dollar, "VariableStorageComponent" )
-	
-	if dollar_comps ~= nil then for i,comp in ipairs( dollar_comps ) do
-		local n = ComponentGetValue2( comp, "name" )
-
-		if n == "gold_value" then
-			ComponentSetValue2( comp, "value_int", math.ceil(money) )
-		elseif n == "hp_value" then
-			if is_blood_money then
-				money = math.max( money, 4 )
-
-				if ModSettingGet( "DEEP_END.HEAVEN_OR_HELL" ) then ComponentSetValue2( comp, "value_float", money * 0.002 )
-				else ComponentSetValue2( comp, "value_float", money * 0.00032 * ( 100 - Random( 25, 50 ) ) ) end
-			end
+		local d_dollar = entity
+		
+		if money >= 1000*1000 then
+			d_dollar = de_load_gold_entity( "data/entities/items/pickup/gold_giant_dollar.xml", x, y-8, remove_timer_level * 4 )
+		elseif money >= 100*1000 then
+			d_dollar = de_load_gold_entity( "data/entities/items/pickup/gold_dollar.xml", x, y-8, remove_timer_level * 3.5 )
+		elseif money >= 10*1000 then
+			d_dollar = de_load_gold_entity( gold_entity .. "10000.xml", x, y-8, remove_timer_level * 3 )
+		elseif money >= 1000 then
+			d_dollar = de_load_gold_entity( gold_entity .. "1000.xml", x, y-8, remove_timer_level * 2.5 )
+		elseif money >= 200 then
+			d_dollar = de_load_gold_entity( gold_entity .. "200.xml", x, y-8, remove_timer_level * 2 )
+		elseif money >= 50 then
+			d_dollar = de_load_gold_entity( gold_entity .. "50.xml", x, y-8, remove_timer_level * 1.5 )
+		else
+			d_dollar = de_load_gold_entity( gold_entity .. "10.xml", x, y-8, remove_timer_level )
 		end
-	end end
+
+		local dollar_comps = EntityGetComponent( d_dollar, "VariableStorageComponent" )
+		
+		if dollar_comps ~= nil then for i,comp in ipairs( dollar_comps ) do
+			local n = ComponentGetValue2( comp, "name" )
+
+			if n == "gold_value" then
+				ComponentSetValue2( comp, "value_int", math.ceil(money) )
+			elseif n == "hp_value" then
+				if is_blood_money then
+					money = math.max( money, 4 )
+
+					if ModSettingGet( "DEEP_END.HEAVEN_OR_HELL" ) then ComponentSetValue2( comp, "value_float", money * 0.002 )
+					else ComponentSetValue2( comp, "value_float", money * 0.00032 * ( 100 - Random( 25, 50 ) ) ) end
+				end
+			end
+		end end
+
+		DEEP_END_MONEY_CD = GameGetFrameNum()
+	end
 end
